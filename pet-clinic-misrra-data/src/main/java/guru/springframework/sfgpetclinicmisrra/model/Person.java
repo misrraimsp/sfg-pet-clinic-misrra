b1d89752565667dbc86846456e53cd6b863c358d
misrraim <misrraimsp@gmail.com>
2019-11-27 14:26:50 +0000

    Adding BaseEntity . Closes #10.

--- pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/model/Person.java
+++ pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinicmisrra.model;
 
-public class Person {
+public class Person  extends BaseEntity{
     private String firstName;
     private String lastName;
 

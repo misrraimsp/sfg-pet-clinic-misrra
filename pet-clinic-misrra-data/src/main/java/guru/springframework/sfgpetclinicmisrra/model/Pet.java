f571e518cbe413bd67634b87858116dedab63995
misrraim <misrraimsp@gmail.com>
2019-12-03 08:07:53 +0000

    Added Visit, updated Owner with Pet relationship. Closes #26.

--- pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/model/Pet.java
+++ pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/model/Pet.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinicmisrra.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
+
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;

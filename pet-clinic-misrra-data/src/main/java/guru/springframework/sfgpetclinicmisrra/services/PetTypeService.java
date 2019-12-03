8a12ac4d9db716e264543dccd4d518245ac563de
misrraim <misrraimsp@gmail.com>
2019-12-03 08:38:18 +0000

    Added PetTypeServiceMap. Closes #29.

---
+++ pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicmisrra.services;
+
+import guru.springframework.sfgpetclinicmisrra.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}

f242d363fa41114a304776344bee927ba73dde68
misrraim <misrraimsp@gmail.com>
2019-12-03 08:15:38 +0000

    Added Specialty, added then to Vets. Closes #27.

---
+++ pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/model/Specialty.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinicmisrra.model;
+
+public class Specialty {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

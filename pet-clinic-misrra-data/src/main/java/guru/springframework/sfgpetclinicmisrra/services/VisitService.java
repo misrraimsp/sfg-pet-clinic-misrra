2e04dfb7391b09eb23ae39e4f12ff972d4db9a4d
misrraim <misrraimsp@gmail.com>
2019-12-11 08:46:19 +0000

    Added Map Service for Visits. Closes #46.

---
+++ pet-clinic-misrra-data/src/main/java/guru/springframework/sfgpetclinicmisrra/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinicmisrra.services;
+
+import guru.springframework.sfgpetclinicmisrra.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
